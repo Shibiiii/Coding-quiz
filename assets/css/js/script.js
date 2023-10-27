00435ed3248d59c1c9f17b148b0a4ac03e536f93
Shibiiii <purplejohn13@gmail.com>
2023-10-27 13:42:34 -0700

    added variables for questions in js

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,10 +1,38 @@
 // some kind of question array
 // object with question and then options array and awnser property
 // {
-//  question: options: [spotify, apple music, physical media, tidal]
+// question: options: [spotify, apple music, physical media, tidal]
 // answer: spotify
-// }
+// };
+var question1 = {
+    question1: 'What is the language used for styling a html page',
+    options: [Javascript, CSS, English, Python],
+    answer: 'CSS'
+};
 
+var question2 = {
+    question2: 'Question 2',
+    options: [Option1, Option2, Option3, Option4],
+    answer: 'Option3'
+};
+
+var question3 = {
+    question2: 'Question 3',
+    options: [Option1, Option2, Option3, Option4],
+    answer: 'Option3'
+};
+
+var question4 = {
+    question2: 'Question 4',
+    options: [Option1, Option2, Option3, Option4],
+    answer: 'Option3'
+};
+
+var question5 = {
+    question2: 'Question 5',
+    options: [Option1, Option2, Option3, Option4],
+    answer: 'Option3'
+};
 // currentQuestion = 0
 
 // question[currentQuestion].question, question[currentQuestion].answer
