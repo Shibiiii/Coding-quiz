b123269ad0db4fda6542b08a16001bc8a5e167eb
Shibiiii <purplejohn13@gmail.com>
2023-10-26 21:35:05 -0700

    basic psuedo coding done in java

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,2 +1,24 @@
 // some kind of question arrsy
 // object with question and then options array and awnser property
+// {
+//  question: options: [spotify, apple music, physaical media, tidal]
+// amswer: spotify
+// }
+
+// currentQuestion = 0
+
+// question[currentQuestion].question, question[currentQuestion].answer
+
+// create question element
+// append option elements
+// event handler => need to check if clicked option is answer
+// does target value === answer
+
+// where are we in question list
+// need to track where with the index
+
+// need a function for starting timer
+// need a function for starting question
+// both need to get called on quiz start
+
+// at the end of the quiz, save time (score) and display highscore
